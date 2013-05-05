7c0f9679de5351e3727006938ad0b891f7cadc53
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-05 02:09:02 -0400

    Did some work on the golfed tokenizer

--- cpp_tokenizer_golfed.py
+++ cpp_tokenizer_golfed.py
@@ -1,14 +1,14 @@
 import re
-with open('i') as f:t=f.read()
+t=open("i").read()
 s=re.findall('".*"|\'.+?\'',t)
 for r in s:t=t.replace(r,'@@')
 t=re.sub('//.*','',t)
-t=re.sub('/\*.+\*/', '',t,0,16)
+t=re.sub('/\*.+\*/','',t,0,16)
 l=t.replace('\t', '').split('\n')
 w=[]
 for i in l:w+=[j for j in i.split(' ') if j]
 r=[]
-for o in w:r+=re.findall('[\d\.]+|;|,|==|=|#|[~<>\+-=%\*\^&\|]+|\w+|\(|\)|{|}|\[|\]|@@|::', o)
+for o in w:r+=re.findall('[\d\.]+|;|,|==|=|#|[~<>\+-=%\*\^&\|]+|\w+|\(|\)|{|}|\[|\]|@@|::',o)
 i=0
 for (j,o) in enumerate(r):
  if o=='@@':
